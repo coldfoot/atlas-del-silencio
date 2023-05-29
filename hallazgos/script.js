0e77e6e5be9da56d71be15c4e1b12c9bc616d025
tiagombp <tiagombp@gmail.com>
2023-05-29 13:38:21 +0200

    finds an optimum value for the projection translation
    did some tests of what factor should be used as the width divisor, for various widths. And it turns out the ratio W / F was always more or less constant and equal to 200! 🙃🤯

--- hallazgos/script.js
+++ hallazgos/script.js
@@ -176,6 +176,8 @@ class Mapa {
         this.w = +window.getComputedStyle(cont).width.slice(0,-2);
         this.h = +window.getComputedStyle(cont).height.slice(0,-2);
 
+        const tx_adjustment_factor = this.w / 200; // I arrived at this "empirically" :/
+
         this.original_viewbox = `0 0 1000 1000`;
 
         this.el.setAttribute('viewBox', this.original_viewbox);
@@ -183,7 +185,7 @@ class Mapa {
         this.proj = d3.geoMercator()
           .center(this.center)
           //.rotate([10, 0])
-          .translate([this.w/3.7, this.h/1.3]) // arrumar um jeito de calcular isso direito
+          .translate([200, this.h/1.3]) // this.w/3.7, // this.w/tx_adjustment_factor // arrumar um jeito de calcular isso direito
           .scale(3600)
 
         ;
