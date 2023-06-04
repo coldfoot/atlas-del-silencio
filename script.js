bb91ec5cd0c849bfb0ec4e3c3b2ca073a32eb410
tiagombp <tiagombp@gmail.com>
2023-06-04 16:39:59 +0200

    makes menu button available before the map loads

--- script.js
+++ script.js
@@ -26,6 +26,152 @@ const main = {
 
 };
 
+class Controls {
+
+    buttons = [
+
+        
+        /*{
+
+            ref : 'btn-reset-map',
+            handler : (e) => {
+
+                console.log('fire');
+                main.mapa.reset_map();
+
+                // precisa resetar o zoom
+
+            }
+        },*/
+
+        /*
+        {
+
+            ref : 'breadcrumb-venezuela',
+            handler : (e) => {
+
+                //console.log('fire');
+                //main.mapa.reset_map();
+                fit_bounds('venezuela');
+
+            }
+        },*/
+
+        {
+
+            ref : 'back-to-main-map',
+            handler : (e) => {
+
+                //console.log('fire');
+                //main.mapa.reset_map();
+                fit_bounds('venezuela');
+
+            }
+        },
+
+        {
+            ref : 'btn-menu',
+            handler : (e) => {
+
+                const menu = document.querySelector('.menu');
+                menu.classList.toggle('hidden');
+
+                const line_top = d3.select('.line-top');
+                const line_mid = d3.select('.line-mid');
+                const line_bottom = d3.select('.line-bottom');
+
+                let top_y2 = main.menuIsExpanded ? '10' : '30';
+                let bottom_y2 = main.menuIsExpanded ? '30' : '10';
+                let mid_opacity = main.menuIsExpanded ? '1' : '0';
+
+                line_top.transition().duration(200).attr('y2', top_y2);
+                line_bottom.transition().duration(200).attr('y2', bottom_y2);
+                line_mid.transition().duration(200).attr('opacity', mid_opacity);
+
+                main.menuIsExpanded = !main.menuIsExpanded;
+
+            }
+        },
+
+        /*
+        {
+
+            ref : 'breadcrumb-provincia',
+            handler : (e) => {
+
+                //console.log('fire');
+                const provincia = document.querySelector('.breadcrumb-provincia').innerText;
+                //console.log(provincia);
+                main.mapa.fit_bounds('provincias', provincia);
+
+            }
+        },*/
+
+        {
+            ref: 'btn-explora',
+            handler: (e) => {
+
+                document.querySelector('.outer-wrapper').dataset.state = "explore";
+
+                utils.getDims();
+                utils.position_back_button();
+
+                fit_bounds('venezuela');
+
+                if (!main.monitoringEstado) monitorEstado('on');
+
+
+
+                //console.log(document.querySelector('.outer-wrapper'));
+
+            }
+        },
+
+
+
+    ];
+
+    refs = {};
+
+    constructor() {
+
+        this.buttons.forEach(button => {
+
+            this.refs[button.ref] = new Button('.' + button.ref, button.handler)
+
+        })
+
+    }
+
+}
+
+class Button {
+
+    ref;
+    el;
+
+    handler;
+
+    constructor(ref, handler) {
+
+        this.ref = ref;
+        this.el = document.querySelector(ref);
+        this.handler = handler;
+        this.monitor();
+
+    }
+
+    monitor() {
+
+        console.log(this.el, ' -- monitoring...');
+        this.el.addEventListener('click', this.handler);
+
+    }
+
+}
+
+main.controls = new Controls();
+
 Promise.all([
 
     fetch(
@@ -82,8 +228,6 @@ function init(data) {
     init_map();
 
     //main.mapa.initZoom();
-
-    main.controls = new Controls();
 
     main.searchBar = new SearchBar('#location-search');
 
@@ -1319,150 +1463,6 @@ class Card {
         })
 
     }
-}
-
-class Controls {
-
-    buttons = [
-
-        
-        /*{
-
-            ref : 'btn-reset-map',
-            handler : (e) => {
-
-                console.log('fire');
-                main.mapa.reset_map();
-
-                // precisa resetar o zoom
-
-            }
-        },*/
-
-        /*
-        {
-
-            ref : 'breadcrumb-venezuela',
-            handler : (e) => {
-
-                //console.log('fire');
-                //main.mapa.reset_map();
-                fit_bounds('venezuela');
-
-            }
-        },*/
-
-        {
-
-            ref : 'back-to-main-map',
-            handler : (e) => {
-
-                //console.log('fire');
-                //main.mapa.reset_map();
-                fit_bounds('venezuela');
-
-            }
-        },
-
-        {
-            ref : 'btn-menu',
-            handler : (e) => {
-
-                const menu = document.querySelector('.menu');
-                menu.classList.toggle('hidden');
-
-                const line_top = d3.select('.line-top');
-                const line_mid = d3.select('.line-mid');
-                const line_bottom = d3.select('.line-bottom');
-
-                let top_y2 = main.menuIsExpanded ? '10' : '30';
-                let bottom_y2 = main.menuIsExpanded ? '30' : '10';
-                let mid_opacity = main.menuIsExpanded ? '1' : '0';
-
-                line_top.transition().duration(200).attr('y2', top_y2);
-                line_bottom.transition().duration(200).attr('y2', bottom_y2);
-                line_mid.transition().duration(200).attr('opacity', mid_opacity);
-
-                main.menuIsExpanded = !main.menuIsExpanded;
-
-            }
-        },
-
-        /*
-        {
-
-            ref : 'breadcrumb-provincia',
-            handler : (e) => {
-
-                //console.log('fire');
-                const provincia = document.querySelector('.breadcrumb-provincia').innerText;
-                //console.log(provincia);
-                main.mapa.fit_bounds('provincias', provincia);
-
-            }
-        },*/
-
-        {
-            ref: 'btn-explora',
-            handler: (e) => {
-
-                document.querySelector('.outer-wrapper').dataset.state = "explore";
-
-                utils.getDims();
-                utils.position_back_button();
-
-                fit_bounds('venezuela');
-
-                if (!main.monitoringEstado) monitorEstado('on');
-
-
-
-                //console.log(document.querySelector('.outer-wrapper'));
-
-            }
-        },
-
-
-
-    ];
-
-    refs = {};
-
-    constructor() {
-
-        this.buttons.forEach(button => {
-
-            this.refs[button.ref] = new Button('.' + button.ref, button.handler)
-
-        })
-
-    }
-
-}
-
-class Button {
-
-    ref;
-    el;
-
-    handler;
-
-    constructor(ref, handler) {
-
-        this.ref = ref;
-        this.el = document.querySelector(ref);
-        this.handler = handler;
-        this.monitor();
-
-    }
-
-    monitor() {
-
-        console.log(this.el, ' -- monitoring...');
-        this.el.addEventListener('click', this.handler);
-
-    }
-
 }
 
 class SearchBar {
